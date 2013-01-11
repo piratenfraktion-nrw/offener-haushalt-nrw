23bf91ac318e506501198eb867272535ede903aa
breeen <breeen@gmail.com>
2013-01-11 16:18:04 +0100

    ie fix(2)

--- protected/extensions/TreemapWidget.php
+++ protected/extensions/TreemapWidget.php
@@ -7,9 +7,18 @@ class TreemapWidget extends CWidget {
 
     public function init() {
 		$this->content = '<div id="infovis"></div>';
+		$_ua = Yii::app()->request->userAgent;
+		if(strpos($_ua, "MSIE 9.0") !== false) {
+			Yii::app()->getClientScript()->registerCssFile(Yii::app()->request->baseUrl . '/assets/treemap_container_ie9.css');
+		} elseif(strpos($_ua, "MSIE 8.0") !== false) {
+			Yii::app()->getClientScript()->registerCssFile(Yii::app()->request->baseUrl . '/assets/treemap_container_ie8.css');
+		} elseif(strpos($_ua, "MSIE 7.0") !== false) {
+			Yii::app()->getClientScript()->registerCssFile(Yii::app()->request->baseUrl . '/assets/treemap_container_ie7.css');
+		} else {
+			Yii::app()->getClientScript()->registerCssFile(Yii::app()->request->baseUrl . '/assets/treemap_container.css');
+		}
 		Yii::app()->getClientScript()->registerCssFile(Yii::app()->request->baseUrl . '/assets/treemap.css');
 		Yii::app()->getClientScript()->registerScriptFile(Yii::app()->request->baseUrl . '/assets/jit.js');
-		//Yii::app()->getClientScript()->registerScriptFile(Yii::app()->request->baseUrl . '/assets/treemap.js');
 		Yii::app()->getClientScript()->registerScriptFile(Yii::app()->request->baseUrl . '/assets/barchart.js');
 		Yii::app()->controller->onloadFunction = 'init(json, json_barchart);';
 
